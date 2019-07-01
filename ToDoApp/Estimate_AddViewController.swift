fe1ed861f829def91064bdb58b8f106ec711d1ce
kiki-ki <malpie0619@gmail.com>
2019-07-02 01:02:49 +0900

    EstimateAddViewController settings

--- ToDoApp/Estimate_AddViewController.swift
+++ ToDoApp/Estimate_AddViewController.swift
@@ -10,12 +10,29 @@ import Foundation
 
 class Estimate_AddViewController: UIViewController{
     
-    @IBOutlet weak var tappedField: UITextField!
     
+    @IBOutlet weak var planField: UITextField!
+    @IBOutlet weak var date_Estimate: UIDatePicker!
     override func viewDidLoad(){
         super.viewDidLoad()
     }
     
     @IBAction func tappedButton(_ sender: Any) {
+        if planField.text != ""{
+            let newItem = Item(initTitle: planField.text!)
+            newItem.date = date_Estimate.date
+            itemArray.append(newItem)
+            //sort
+            itemArray = itemArray.sorted(by: { (a, b) -> Bool in
+                return a.date < b.date
+            })
+            planField.text = ""
+            self.navigationController?.popViewController(animated: true)
+        } else{
+            let alert = UIAlertController(title: "入力エラー！", message: "アイテムを入力して下さい", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+        }
     }
 }
