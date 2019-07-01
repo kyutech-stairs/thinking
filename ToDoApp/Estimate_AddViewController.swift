94e72378335144e12b8f1e65ce854968f7bc7165
kiki-ki <malpie0619@gmail.com>
2019-07-02 00:11:28 +0900

    madeEstimate_AddViewController.swift file and connected Button and TextField for test.storyboard

---
+++ ToDoApp/Estimate_AddViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Estimate_AddViewController.swift
+//  ToDoApp
+//
+//  Created by kai nakata on 2019/07/01.
+//  Copyright © 2019 山之内一隆. All rights reserved.
+//
+import UIKit
+import Foundation
+
+class Estimate_AddViewController: UIViewController{
+    
+    @IBOutlet weak var tappedField: UITextField!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tappedButton(_ sender: Any) {
+    }
+}
