5915fe74cb28df8620230ae54726a048cb47fd2d
kiki-ki <malpie0619@gmail.com>
2019-07-06 12:35:41 +0900

    Home.storyboard can transition to other storyboards

--- ToDoApp/AddHomeViewController.swift
+++ ToDoApp/AddHomeViewController.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2019 山之内一隆. All rights reserved.
 //
 
+import UIKit
 import Foundation
+
+class AddHomeViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
--- ToDoApp/HomeViewController.swift
+++ ToDoApp/HomeViewController.swift
@@ -5,5 +5,13 @@
 //  Created by kai nakata on 2019/07/06.
 //  Copyright © 2019 山之内一隆. All rights reserved.
 //
-
+import UIKit
 import Foundation
+class HomeViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
